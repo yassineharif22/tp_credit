4dd6d5092a1892724b2b61bb5ecacb4ef6de3758
yassineharif22 <yassineharif22@gmail.com>
2026-03-24 12:22:27 +0100

    interface.py update2

--- app.py
+++ app.py
@@ -227,5 +227,5 @@ def not_found(error):
 # ============================================
 # ✅ Compatible déploiement
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port, debug=True)
